4c5bd500fa015d83263d1f4f9ba3b8105e806e7a
sofia0629 <sofiakang0629@gmail.com>
2026-03-03 16:31:09 +0900

    Fix typo in text input function name

--- streamlit_main.py
+++ streamlit_main.py
@@ -10,5 +10,5 @@ import seaborn as sns
 import re
 import streamlit as st
 
-company_name= st.text_import("회사명을 입력해 주세요", placeholder= '검색할 회사명 입력')
+company_name= st.text_input("회사명을 입력해 주세요", placeholder= '검색할 회사명 입력')
 
